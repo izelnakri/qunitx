4ce741fbc8333fe68a395effc5432f4d36a45b5e
Izel Nakri <contact@izelnakri.com>
2021-03-04 02:02:40 +0100

    todo tests added

---
+++ test/flags/error-cases-test.js
@@ -0,0 +1,8 @@
+// TODO: invalid extension, use --debug without browser
+// folder/file doesnt exist to watch
+// unknown command
+import test from 'ava';
+
+test('todo', async (t) => {
+  t.true(true);
+});
---
+++ test/folder-test.js
@@ -0,0 +1,5 @@
+import test from 'ava';
+
+test('todo', async (t) => {
+  t.true(true);
+});
