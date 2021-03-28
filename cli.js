0b4296788df618f52b57879c22e60d38a9926302
Izel Nakri <contact@izelnakri.com>
2021-03-28 14:49:35 +0200

    read and set config on package.json

--- cli.js
+++ cli.js
@@ -1,9 +1,11 @@
 #! /usr/bin/env node
 import fs from 'fs/promises';
+import defaultProjectConfigValues from './lib/boilerplates/default-project-config-values.js';
 import displayHelpOutput from './lib/commands/help.js';
 import initializeProject from './lib/commands/init.js';
 import generateTestFiles from './lib/commands/generate.js';
 import run from './lib/commands/run.js';
+import findProjectRoot from './lib/utils/find-project-root.js';
 import parseCliFlags from './lib/utils/parse-cli-flags.js';
 import resolvePortNumberFor from './lib/utils/resolve-port-number-for.js';
 
@@ -20,14 +22,25 @@ process.title = 'qunitx';
     return await initializeProject();
   }
 
-  let config = await parseCliFlags();
+  let [config, { projectRoot, packageJSONConfig }] = await Promise.all([
+    parseCliFlags(),
+    readConfigFromPackageJSON()
+  ]);
+  let targetConfig = Object.assign(
+    { htmlPaths: [] },
+    defaultProjectConfigValues,
+    packageJSONConfig.qunitx,
+    config,
+    { projectRoot }
+  );
 
-  Object.assign(config, {
-    httpPort: config.httpPort || 1234,
-    timeout: config.timeout || 10000,
-    output: config.output|| `${config.projectRoot}/tmp`,
-    htmlPaths: config.htmls || [`${config.projectRoot}/test/tests.html`] // TODO: add this to package.json
-  });
-
-  return await run(config);
+  return await run(targetConfig);
 })();
+
+async function readConfigFromPackageJSON() {
+  const projectRoot = await findProjectRoot();
+  const packageJSON = await fs.readFile(`${projectRoot}/package.json`);
+  const packageJSONConfig = JSON.parse(packageJSON.toString());
+
+  return { projectRoot, packageJSONConfig };
+}
