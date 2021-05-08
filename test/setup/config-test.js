af4dacf8d52ff46de58b3087976ccd1c4172b91c
Izel Nakri <contact@izelnakri.com>
2021-05-08 19:20:21 +0200

    glob filter algorithm final implementation

--- test/setup/config-test.js
+++ test/setup/config-test.js
@@ -2,16 +2,19 @@ import assert from 'assert';
 import { buildWatchPaths } from '../../lib/setup/config.js';
 
 describe('Setup | config tests', function() {
-  it('buildWatchPaths() works correctly on different inputs', async function() {
+  it('buildWatchPaths() works correctly on different inputs', function() {
     let projectRoot = '/home/izelnakri/Github/qunitx';
 
     assert.deepEqual(buildWatchPaths(projectRoot, [
       `${projectRoot}/tmp`,
       `${projectRoot}/tmp/vendor`,
+      `${projectRoot}/another/first/*`,
+      `${projectRoot}/another/first/something/helpers`,
       `${projectRoot}/tmp/build-*`,
       `${projectRoot}/vendor`,
       `${projectRoot}/vendor-*`,
       `${projectRoot}/vendor-*/files`,
+      `${projectRoot}/tests/**/something/*-test.ts`,
       `${projectRoot}/tests/**/*.ts`,
       `${projectRoot}/assets/something-test.ts`,
       `${projectRoot}/assets/*-test.js`,
@@ -19,8 +22,9 @@ describe('Setup | config tests', function() {
       `${projectRoot}/vendor/*-test.js`,
     ]), [
       `${projectRoot}/tmp`,
-      `${projectRoot}/vendor-*`,
+      `${projectRoot}/another/first/*`,
       `${projectRoot}/vendor`,
+      `${projectRoot}/vendor-*`,
       `${projectRoot}/tests/**/*.ts`,
       `${projectRoot}/assets/*-test.js`,
       `${projectRoot}/assets/something-test.ts`,
