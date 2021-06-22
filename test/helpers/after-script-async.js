2930cd617c474117f99fb25c005bcdb7eaf3550d
Izel Nakri <contact@izelnakri.com>
2021-06-22 10:05:56 +0200

    fix test cleanup

--- test/helpers/after-script-async.js
+++ test/helpers/after-script-async.js
@@ -4,7 +4,7 @@ import './after-script-basic.js';
 export default async function(results) {
   let resultsInString = JSON.stringify(results, null, 2);
 
-  await fs.rm(`${process.cwd()}/tmp/results.json`, { force: true });
+  await fs.rm(`${process.cwd()}/tmp/results.json`, { force: true, recursive: true });
   await fs.writeFile(`${process.cwd()}/tmp/results.json`, resultsInString);
 
   console.log('After script result is written:');
