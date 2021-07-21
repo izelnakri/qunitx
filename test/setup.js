3ef8d94d4cb9f8f505f8ebfede344c585cf80748
Izel Nakri <contact@izelnakri.com>
2021-07-21 09:26:10 +0200

    remove redundant fs.mkdir option

--- test/setup.js
+++ test/setup.js
@@ -3,7 +3,7 @@ import fs from 'fs/promises';
 process.env['FORCE_COLOR'] = 0;
 
 await fs.rm('./tmp', { recursive: true, force: true });
-await fs.mkdir('./tmp/test', { recursive: true, force: true });
+await fs.mkdir('./tmp/test', { recursive: true });
 let [failingTestContent, passingTestContent] = await Promise.all([
   fs.readFile('./test/helpers/failing-tests.js'),
   fs.readFile('./test/helpers/passing-tests.js'),
