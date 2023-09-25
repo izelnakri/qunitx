d1a8870397aab4ec6f51fbbc5ca9d04558a7f6da
Izel Nakri <contact@izelnakri.com>
2023-09-25 02:26:57 +0200

    Make flake run forAllSystems supported

--- flake.nix
+++ flake.nix
@@ -3,25 +3,31 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.napalm.url = "github:nix-community/napalm";
-
-  # NOTE: This is optional, but is how to configure napalm's env
   inputs.napalm.inputs.nixpkgs.follows = "nixpkgs";
 
   outputs = { self, nixpkgs, napalm }:
-  let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages."${system}";
-  in {
-    packages."${system}".default = napalm.legacyPackages."${system}".buildPackage ./. {
-      nodejs = pkgs.nodejs_20;
-      PUPPETEER_SKIP_DOWNLOAD=1;
-    };
+    let
+      system = "x86_64-linux";
+      supportedSystems = [ "x86_64-linux" "i686-linux" "aarch64-linux" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+      pkgs = nixpkgs.legacyPackages."${system}";
+    in {
+      packages = forAllSystems (system: rec {
+        inherit system;
+        default = napalm.legacyPackages."${system}".buildPackage ./. {
+          nodejs = pkgs.nodejs_20;
+          PUPPETEER_SKIP_DOWNLOAD=1;
+        };
+      });
 
-    devShells."${system}".default = pkgs.mkShell {
-      nativeBuildInputs = with pkgs; [
-        deno
-        nodejs_20
-      ];
+      devShells = forAllSystems (system: rec {
+        inherit system;
+        default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            deno
+            nodejs_20
+          ];
+        };
+      });
     };
-  };
 }
