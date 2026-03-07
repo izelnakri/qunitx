b4d8dbeccabb31d7fa10e08d0dce41d1f318b1b0
Izel Nakri <contact@izelnakri.com>
2026-03-07 01:19:49 +0100

    dev: update flake.nix for develop shell

--- flake.nix
+++ flake.nix
@@ -1,32 +1,54 @@
 {
   description = "A flake for qunitx npm package";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
   inputs.napalm.url = "github:nix-community/napalm";
   inputs.napalm.inputs.nixpkgs.follows = "nixpkgs";
 
   outputs = { self, nixpkgs, napalm }:
     let
-      system = "x86_64-linux";
       supportedSystems = [ "x86_64-linux" "i686-linux" "aarch64-linux" ];
-      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
-      pkgs = nixpkgs.legacyPackages."${system}";
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system nixpkgs.legacyPackages."${system}");
     in {
-      packages = forAllSystems (system: rec {
-        inherit system;
+      packages = forAllSystems (system: pkgs: rec {
         default = napalm.legacyPackages."${system}".buildPackage ./. {
           nodejs = pkgs.nodejs_20;
           PUPPETEER_SKIP_DOWNLOAD=1;
         };
       });
 
-      devShells = forAllSystems (system: rec {
-        inherit system;
+      devShells = forAllSystems (system: pkgs: {
         default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            deno
-            nodejs_20
+          nativeBuildInputs = [
+            pkgs.pkg-config
+            pkgs.deno
+            pkgs.nodejs_20
+            self.packages."${system}".default.nativeBuildInputs
+            self.packages."${system}".default.buildInputs
           ];
+
+          doCheck = false; # Disables automatically running tests for `$ nix develop` and direnv
+
+          shellHook = ''
+            export ZDOTDIR=$(mktemp -d)
+            cat > "$ZDOTDIR/.zshrc" << 'EOF'
+              source ~/.zshrc # Source the original ~/.zshrc, required.
+              function parse_git_branch {
+                git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\ ->\ \1/'
+              }
+              function display_jobs_count_if_needed {
+                local job_count=$(jobs -s | wc -l | tr -d " ")
+                if [ $job_count -gt 0 ]; then
+                  echo "%B%F{yellow}%j| ";
+                fi
+              }
+              # NOTE: Custom prompt with a snowflake: signals we are in `$ nix develop` shell
+              PROMPT="%F{blue}$(date +%H:%M:%S) $(display_jobs_count_if_needed)%B%F{green}%n %F{blue}%~%F{cyan} ❄%F{yellow}$(parse_git_branch) %f%{$reset_color%}"
+            EOF
+            if [ -z "$DIRENV_IN_ENVRC" ]; then # This makes `$ nix develop` universally working with direnv without infinite loop
+              exec ${pkgs.zsh}/bin/zsh -i
+            fi
+          '';
         };
       });
     };
